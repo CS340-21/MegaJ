1b66d2b77bd96066f5ecb10b1f4e4457af3653b7
JGray17 <46678600+JGray17@users.noreply.github.com>
2021-03-17 17:47:53 -0400

    Add files via upload

---
+++ MissionRecipe/MissionRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MissionRecipeApp.swift
+//  MissionRecipe
+//
+//  Created by Joshua Gray on 3/17/21.
+//
+
+import SwiftUI
+
+@main
+struct MissionRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
