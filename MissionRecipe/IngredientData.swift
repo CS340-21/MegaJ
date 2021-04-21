72816e1e837fadfc57442f3652ee1371d7c5e713
mstanto4 <mstanto4@vols.utk.edu>
2021-04-21 10:40:35 -0400

    More Ingredients

--- MissionRecipe/IngredientData.swift
+++ MissionRecipe/IngredientData.swift
@@ -8,12 +8,30 @@ var ingredientData: [Ingredient] = [
         name: "Salmon",
         expiration: 5,
         type: "Fish",
-        comments: "2 Portions"
+        comments: "4 Portions"
+        ),
+    Ingredient(
+        name: "Black Pepper",
+        expiration: 356,
+        type: "Seasoning",
+        comments: "None"
+        ),
+    Ingredient(
+        name: "Lemon",
+        expiration: 10,
+        type: "Fruit",
+        comments: "Amount: 7"
+        ),
+    Ingredient(
+        name: "Mayonnaise",
+        expiration: 25,
+        type: "Condiment",
+        comments: "Jar"
         ),
     Ingredient(
         name: "Avocado",
         expiration: 3,
         type: "Fruit",
-        comments: "10 of them"
+        comments: "Amount: 3"
         )
 ]
--- MissionRecipe/RecipeData.swift
+++ MissionRecipe/RecipeData.swift
@@ -12,21 +12,23 @@ var recipesData: [Recipe] = [
     cookTime: 15,
     instructions: [
         "Preheat air fryer to 400°F.",
-        "In a shallow bowl combine the egg and hot sauce.",
-        "Combine garlic powder, paprika, chili powder, black pepper, bread crumb, and panko in a medium bowl and toss well.",
+        "In a shallow bowl combine one egg and 3 tablespoons of hot sauce.",
+        "Combine 1/2 tsp garlic powder, 1/2 tsp paprika, 1/2 tsp chili powder, 1/8 tsp black pepper, 6 tbsp of bread crumb, and 1/4 cup of panko in a medium bowl and toss well.",
+        "Cut 1 lb of chicken into 32 small bite sized pieces.",
         "Dip the chicken in the hot sauce egg mix, then into the crumbs and place on a work surface and spray with oil.",
         "Add in a single layer to the air fryer and cook 6-8 minutes turning halfway. Repeat wtih additional batches."
     ],
     ingredients: [
-        "1 egg",
-        "1/2 tsp garlic powder",
-        "1/2 tsp chili powder (use ground cayenne to make it hotter)",
-        "1/8 tsp black pepper",
-        "6 tbsp whole wheat Italian seasoned breadcrumbs",
-        "1/4 cup panko",
-        "olive oil spray",
-        "3 tbsp Frank's Hot Sauce (or Tabasco if you want it hot",
-        "1 lb skinless chicken breast (cut into 32 small bite sized pieces)"
+        "Egg",
+        "Garlic Powder",
+        "Paprika",
+        "Chili Powder",
+        "Black Pepper",
+        "Italian Seasoned Breadcrumbs",
+        "Panko",
+        "Olive Oil Spray",
+        "Hot Sauce",
+        "Chicken"
     ]
     ),
     Recipe(
@@ -39,26 +41,26 @@ var recipesData: [Recipe] = [
         instructions: [
             "Air Fryer Directions:",
             "1. Preheat air fryer to 400°F. Spritz the basket with olive oil.",
-            "2. Season the salmon with lemon juice, salt and pepper.",
-            "3. Mix the mayonnaise with basil and 2 tablespoons Parmesan cheese in a small bowl.",
-            "4. Spread completely over the top of salmon. Sprinkle remaining Parmesan cheese on top.",
+            "2. Season the 4 salmon filets with lemon juice, 1/4 tsp salt and pepper.",
+            "3. Mix the 3 tbsp of mayonnaise with 6 minced basil leaves and 2 tablespoons Parmesan cheese in a small bowl.",
+            "4. Spread completely over the top of salmon. Sprinkle the remaining tablespoon of Parmesan cheese on top.",
             "5. In batches, air fry 7 minutes, or longer depending on thickness of salmon.",
             "Oven Directions:",
             "1. Preheat the oven to 425°F. Spritz a sheet pan with olive oil.",
-            "2. Season the salmon with lemon juice, salt and pepper.",
-            "3. Mix the mayonnaise with basil and 2 tablespoons Parmesan cheese in a small bowl.",
-            "4. Spread completely over the top of salmon. Sprinkle remaining Parmesan cheese on top.",
+            "2. Season the 4 salmon filets with lemon juice, 1/4 tsp salt and pepper.",
+            "3. Mix the 3 tbsp of mayonnaise with 6 minced basil leaves and 2 tablespoons Parmesan cheese in a small bowl.",
+            "4. Spread completely over the top of salmon. Sprinkle remaining tablespoon of Parmesan cheese on top.",
             "5. Bake 8 to 12 minutes, depending on thickness of the salmon."
         ],
         ingredients: [
-            "olive oil spray",
+            "Olive Oil Spray",
             "Salmon",
-            "1/2 lemon",
-            "1/4 tsp kosher salt",
-            "freshly ground black pepper",
-            "3 tbsp mayonnaise",
-            "6 fresh basil leaves (minced, plus more for garnish)",
-            "3 tbsp grated parmesan or romano cheese"
+            "Lemon",
+            "Salt",
+            "Black Pepper",
+            "Mayonnaise",
+            "Basil",
+            "Grated Parmesan"
         ]
     )
 ]
